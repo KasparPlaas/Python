e159b8b2485755ef941caf380fe3073547d9687b
KasparPlaas <kaspar.plaas@hkhk.edu.ee>
2024-01-30 13:53:17 +0200

    Update Harjutus1.py

--- PyGame/Harjutus1.py
+++ PyGame/Harjutus1.py
@@ -1,16 +1,20 @@
 import pygame
 pygame.init()
+#ekraani seaded
+screen=pygame.display.set_mode([640,480])
+pygame.display.set_caption("Harjutamine")
+screen.fill([204, 255, 204])
  
-# Ekraani seaded
-screen=pygame.display.init() 
-pygame.display.set_mode([300,300])
-pygame.display.set_caption("Lumemees - Kaspar Plaas")
-
-screen.fill([0,0,0])
-
-#Joonistamine
-pygame.draw.circle(screen, [255, 255, 255], [150, 250], 100, 1)
-
+#lisame teksti
+font = pygame.font.Font(pygame.font.match_font('arial'), 50)
+font.set_underline(True)
+text = font.render("Hello PyGame", True, [0,0,0])
+ 
+#tekstikasti suurus
+text_width = text.get_rect().width
+text_height = text.get_rect().height
+ 
+screen.blit(text, [320-text_width/2,240-text_height/2])
+ 
 pygame.display.flip()
-
-
+ 
